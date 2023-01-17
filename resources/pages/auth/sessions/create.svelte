f57bdf860f4e77c8a03ead1e2b950bd24b3493c2
Jaggy <jaggy@humans.ph>
2023-01-17 14:59:38 +0800

    Add prettier and eslint

--- resources/pages/auth/sessions/create.svelte
+++ resources/pages/auth/sessions/create.svelte
@@ -1,21 +1,21 @@
 <script>
-    import {useForm} from "@inertiajs/svelte";
+    import { useForm } from "@inertiajs/svelte"
 
     export let links
 
     const form = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
     })
 
-    function submit () {
+    function submit() {
         $form.post(links.store_path)
     }
 </script>
 
 <form action={links.store_path} method="POST" on:submit|preventDefault={submit}>
-    <input type="email" bind:value={$form.email}>
-    <input type="password" bind:value={$form.password}>
+    <input type="email" bind:value={$form.email} />
+
+    <input type="password" bind:value={$form.password} />
     <button type="submit">Signin</button>
 </form>
-
